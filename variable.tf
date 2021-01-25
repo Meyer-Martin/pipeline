f744db43b4e1d16c8297cbddb399e8cbedf070b9
MEYER <51441370+Meyer-Martin@users.noreply.github.com>
2021-01-25 15:16:37 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,20 @@ variable "instance_type" {
   default = "t2.large"
 }
 
+variable "instance_number" {
+  type = number
+  description = "Instance number"
+  default = 1
+}
+
+variable "create_instance" {
+  type = bool
+  description = "Create instance or not"
+  default = true
+}
+
+variable "bucket" {
+  type = string
+  description = "Terraform state bucket"
+  default = "ec2_terraform"
+}
