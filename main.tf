e88ee37247a41023e93b86d39bbf8e895d4cf48c
MEYER <51441370+Meyer-Martin@users.noreply.github.com>
2021-01-25 15:13:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
+  count         = var.create_instance ? var.instance_number : 0
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name   = "tp_dev_ynov"
